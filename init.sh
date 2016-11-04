05fafcd39f3a9fef15cc5f63e0622bc543977b37
Nick Desaulniers <wangstabill@snet.net>
2016-11-03 19:55:50 -0700

    cant add them as submodules...

--- init.sh
+++ init.sh
@@ -2,3 +2,11 @@ set -xe
 git submodule init
 git submodule update --depth 1
 cp build/core/root.mk Makefile
+
+cd build
+git clone --depth 1 https://android.googlesource.com/platform/build/soong
+git clone --depth 1 https://android.googlesource.com/platform/build/blueprint
+cd -
+# http://stackoverflow.com/a/37629211/1027966
+ln -s build/soong/bootstrap.bash
+ln -s build/soong/Android.bp
