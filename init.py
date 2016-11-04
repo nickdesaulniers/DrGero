3c731dd234bb6540efea796e39d67ec07d015e1e
Nick Desaulniers <wangstabill@snet.net>
2016-11-03 19:12:06 -0700

    initial commit

---
+++ init.py
@@ -0,0 +1,31 @@
+import subprocess
+import urlparse
+
+repos = [
+    (
+        "https://android.googlesource.com/platform/frameworks/native",
+        "frameworks/native"
+    ), (
+        "https://android.googlesource.com/platform/build",
+        "build"
+    ), (
+        "https://android.googlesource.com/device/lge/hammerhead",
+        "device/lge/hammerhead"
+    ), (
+        "https://android.googlesource.com/device/lge/hammerhead-kernel",
+        "device/lge/hammerhead-kernel"
+    )
+]
+
+cmds = [
+    "git",
+    "submodule",
+    "add",
+    "--depth",
+    "1"
+]
+
+subprocess.call(["git", "init"])
+for url, path in repos:
+    subprocess.call(cmds + [url, path])
+subprocess.call(["cp", "build/core/root.mk", "Makefile"])
