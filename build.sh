e9e414151b8dff707cea459418ea3aff8e04c343
Nick Desaulniers <wangstabill@snet.net>
2016-11-03 19:45:43 -0700

    cp Makefile

--- build.sh
+++
@@ -1,4 +0,0 @@
-set -xe
-source build/envsetup.sh
-lunch aosp_hammerhead-userdebug
-make -j bootimage
--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
 set -xe
 git submodule init
 git submodule update --depth 1
+cp build/core/root.mk Makefile
