5cc3f75789cb39589ab129ffff079cd60fb1f955
Nick Desaulniers <wangstabill@snet.net>
2016-11-05 20:24:06 -0700

    allow reinit, add kati

--- init.sh
+++ init.sh
@@ -1,12 +1,23 @@
 set -xe
+
+# clone a repo only if it doesn't exist yet.
+# allows you to rerun this script without fail.
+check_clone() {
+  dir=$(basename $1)
+  if [ ! -d $dir ]; then
+    echo "cloning $1"
+    git clone --depth 1 $1
+  fi
+}
+
 git submodule init
 git submodule update --depth 1
 cp build/core/root.mk Makefile
 
 cd build
-git clone --depth 1 https://android.googlesource.com/platform/build/soong
-git clone --depth 1 https://android.googlesource.com/platform/build/blueprint
-cit clone --depth 1 git clone https://android.googlesource.com/platform/build/kati
+check_clone https://android.googlesource.com/platform/build/soong
+check_clone https://android.googlesource.com/platform/build/blueprint
+check_clone https://android.googlesource.com/platform/build/kati
 cd -
 # http://stackoverflow.com/a/37629211/1027966
 ln -s build/soong/bootstrap.bash
